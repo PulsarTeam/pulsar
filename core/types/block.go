037a5b38ea87f59209f471feeac720ae1cc27a07
Unknown <dts7486@163.com>
2018-09-13 01:57:47 -0700

    stake reward calucate and accumulate

--- core/types/block.go
+++ core/types/block.go
@@ -83,6 +83,8 @@ type Header struct {
 	Extra       []byte         `json:"extraData"        gencodec:"required"`
 	MixDigest   common.Hash    `json:"mixHash"          gencodec:"required"`
 	Nonce       BlockNonce     `json:"nonce"            gencodec:"required"`
+	PosProduction *big.Int     `json:"posProduction"    gencodec:"required"`
+	PowProduction *big.Int     `json:"powProduction"    gencodec:"required"`
 }
 
 // field type overrides for gencodec
