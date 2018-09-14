3f34b090b172ef9f6f572162703795e7ce9d7d88
wuchuanhui <wuchuanhui@kg.com>
2018-09-14 00:36:47 -0700

    添加获取委托信息的接口

---
+++ core/delegateminers/delegateminers.go
@@ -0,0 +1,17 @@
+package delegateminers
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
+)
+
+type Depositor struct {
+	addr common.Address
+	amount    big.Int
+}
+
+
+func getDepositors(address common.Address)([]Depositor, error)  {
+	depositors := []Depositor{}
+	return depositors,nil
+}
