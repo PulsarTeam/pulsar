7af7cd53955107526370c5eb587bbe45452d7fab
linc <lincheng@kg.com>
2018-10-09 19:58:43 -0700

    update .gitignore

--- build/start-full-node.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#bin/geth -rpc -rpcapi 'web3,eth,debug,personal' -rpcport 8545 --rpccorsdomain '*' --datadir ~/data/ --networkid 15 --verbosity 6 --nodiscover console
-bin/geth -rpc -rpcapi 'web3,eth,debug,personal' -rpcport 8545 --rpccorsdomain '*' --datadir ~/data/ --networkid 15 --verbosity 2 console
