2e1d49fd3ccdbced97f150eb44f651ff69ac2824
chuanhui812 <chuanhui812@gmail.com>
2018-10-14 20:29:08 -0700

    modify only block number mod cycle is zero query the depositors

--- core/delegateminers/delegateminers.go
+++ core/delegateminers/delegateminers.go
@@ -1,59 +1,66 @@
 package delegateminers
 
 import (
+	"errors"
 	"github.com/ethereum/go-ethereum/common"
-	"math/big"
-	"github.com/ethereum/go-ethereum/core/state"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/consensus"
 	"github.com/ethereum/go-ethereum/consensus/availabledb"
-	"errors"
+	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/types"
+	"math/big"
 )
 
 type Depositor struct {
-	Addr common.Address
-	Amount    *big.Int
+	Addr   common.Address
+	Amount *big.Int
 }
 
 type DelegateMiner struct {
 	Depositors []Depositor
-	Fee uint32
+	Fee        uint32
 }
 
-func GetDelegateMiner(ethash *availabledb.AvailableDb, chain consensus.ChainReader, header *types.Header, address common.Address)(*state.StateDB,*DelegateMiner, error)  {
+var delegateMiner = DelegateMiner{}
+
+func GetDelegateMiner(ethash *availabledb.AvailableDb, chain consensus.ChainReader, header *types.Header, address common.Address) (*state.StateDB, *DelegateMiner, error) {
 	var err error = nil
 
-	var state, stateErr = ethash.GetAvailableDb(chain,header)
-    if state == nil {
-        return nil, nil, stateErr
+	var state, stateErr = ethash.GetAvailableDb(chain, header)
+	if state == nil {
+		return nil, nil, stateErr
 	}
+	cylce := header.Number.Uint64() / ethash.DsPowCycle
+	cylcemod := header.Number.Uint64() % ethash.DsPowCycle
 
-	var depositorMap = state.GetDepositUsers(address)
-	var miners = state.GetAllDelegateMiners()
-	miner ,ok := miners[address]
-	if (!ok){
-		err = errors.New(`no miner!`)
+	if cylce >= 2 && cylcemod == 0 {
+		delegateMiner.Depositors = delegateMiner.Depositors[:0:0]
+		var depositorMap = state.GetDepositUsers(address)
+		var miners = state.GetAllDelegateMiners()
+		miner, ok := miners[address]
+		if !ok {
+			err = errors.New(`no miner!`)
+		}
+		delegateMiner.Fee = miner.FeeRatio
+		for k, v := range depositorMap {
+			depositor := Depositor{Addr: k, Amount: v.Balance}
+			delegateMiner.Depositors = append(delegateMiner.Depositors, depositor)
+		}
 	}
-	delegateMiner := &DelegateMiner{Fee: miner.FeeRatio}
-	for k, v := range depositorMap{
-		depositor := Depositor{Addr:k, Amount:v.Balance}
-		delegateMiner.Depositors = append(delegateMiner.Depositors, depositor)
-	}
-	return state, delegateMiner,err
+	return state, &delegateMiner, err
 }
 
-func GetLastCycleDepositAmount(state *state.StateDB)(*big.Int,error)  {
+func GetLastCycleDepositAmount(state *state.StateDB) (*big.Int, error) {
 	var err error = nil
 	var miners = state.GetAllDelegateMiners()
 	var amount uint64
-	for _, v := range miners{
+	for _, v := range miners {
 		amount += v.DepositBalance.Uint64()
 	}
-	return new(big.Int).SetUint64(amount),err
+	return new(big.Int).SetUint64(amount), err
 }
 
-func GetLastCycleDelegateMiners(state *state.StateDB)(uint64,error)  {
+func GetLastCycleDelegateMiners(state *state.StateDB) (uint64, error) {
 	var miners = state.GetAllDelegateMiners()
 	var minersAmount = len(miners)
-	return (uint64)(minersAmount),nil
+	return (uint64)(minersAmount), nil
 }
