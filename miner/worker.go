1fcd7a57984faf84699ef16bfce60bee8dca8023
chuanhui812 <chuanhui812@gmail.com>
2018-11-29 14:02:58 +0800

    modify gaslimit calculation method and delete redundant code

--- miner/worker.go
+++ miner/worker.go
@@ -477,6 +477,7 @@ func (self *worker) commitNewWork() {
 	}
 	for i := 0; i < len(uncles); i++ {
 		work.header.GasLimit += uncles[i].GasLimit
+		header.GasLimit += uncles[i].GasLimit
 	}
 	// the result tx with account
 	txWithAcc := make(map[common.Address]types.Transactions)
@@ -667,71 +668,4 @@ func (env *Work) commitTransaction(tx *types.Transaction, bc *core.DAGManager, c
 	env.receipts = append(env.receipts, receipt)
 
 	return nil, receipt.Logs
-}
-
-type TxWithAccount struct {
-	account common.Address
-	tx      *types.Transaction
-	removed bool
-}
-
-func newTxWithAccount(account common.Address, tx *types.Transaction) *TxWithAccount {
-	return &TxWithAccount{
-		account: account,
-		tx:      tx,
-		removed: false,
-	}
-}
-
-func (self *worker) handleConflictTransactions(block *types.Block, uncles []*types.Block) types.Transactions {
-	var orderedTxs, unclesTxs types.Transactions
-	var txsWithAccount []*TxWithAccount
-
-	pivotTxs := block.Transactions()
-
-	if len(uncles) == 0 {
-		orderedTxs = pivotTxs
-	} else {
-		unclesTxs = uncles[0].Transactions()
-		for _, initTx := range unclesTxs {
-			// Get the sender address of tx from the signer
-			account, _ := types.Sender(self.current.signer, initTx)
-			txWithAccount := newTxWithAccount(account, initTx)
-			txsWithAccount = append(txsWithAccount, txWithAccount)
-		}
-		if len(uncles) > 1 {
-			for i, uncle := range uncles {
-				if i == 0 {
-					break
-				}
-				for _, uncleTx := range uncle.Transactions() {
-					txsWithAccount = self.validateTxWithAccount(uncleTx, txsWithAccount)
-				}
-			}
-		}
-	}
-
-	for _, pivotTx := range pivotTxs {
-		txsWithAccount = self.validateTxWithAccount(pivotTx, txsWithAccount)
-	}
-
-	for _, txWithAccount := range txsWithAccount {
-		if !txWithAccount.removed {
-			orderedTxs = append(orderedTxs, txWithAccount.tx)
-		}
-	}
-
-	return orderedTxs
-}
-
-func (self *worker) validateTxWithAccount(tx *types.Transaction, txsWithAccount []*TxWithAccount) []*TxWithAccount {
-	account, _ := types.Sender(self.current.signer, tx)
-	for _, txWithAccount := range txsWithAccount {
-		if account == txWithAccount.account && tx.Nonce() == txWithAccount.tx.Nonce() && tx.GasPrice().Cmp(txWithAccount.tx.GasPrice()) > 0 {
-			txWithAccount.removed = true
-		}
-		newTxWithAccount := newTxWithAccount(account, tx)
-		txsWithAccount = append(txsWithAccount, newTxWithAccount)
-	}
-	return txsWithAccount
 }
