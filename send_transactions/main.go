30928fc6e2c4946894ec087eaa252af74cc9ff1c
lester <lester123@live.cn>
2018-12-17 22:45:11 -0800

    send transactions for testing

---
+++ send_transactions/main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/rpc"
+	"math/big"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+)
+
+//Count send complete
+var Count int64
+
+//Transaction from to account id
+var from, to, frequency = 0, 0, 1
+
+//Two transmission intervals
+var interval = time.Millisecond * 0
+
+//get all account
+var account []string
+
+// The message state
+var msg = make(chan bool)
+
+// Restart the number
+var num int
+
+// SLEEPTIME The interval between reconnections
+const SLEEPTIME = 120
+
+// get par
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Printf("invalid args : %s [count] [frequency] [interval] [from] [to] [\"ip:port\"]\n", os.Args[0])
+		return
+	}
+
+	count, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Println("count err")
+		return
+	}
+
+	frequency, err = strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("frequency err")
+		return
+	}
+
+	intervalCount, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		fmt.Println("interval err")
+		return
+	}
+
+	interval = time.Millisecond * time.Duration(intervalCount)
+
+	from, err = strconv.Atoi(os.Args[4])
+	if err != nil {
+		fmt.Println("from err default 0")
+	}
+
+	to, err = strconv.Atoi(os.Args[5])
+
+	if err != nil {
+		fmt.Println("to err 0：Local address 1: Generate address")
+	}
+
+	ip := "127.0.0.1:8545"
+	if len(os.Args) == 7 {
+		ip = os.Args[6]
+	}
+	fmt.Println("==========================")
+
+	go send(count, ip)
+
+	for {
+		if !<-msg {
+			fmt.Println("======================send Transaction restart=========================")
+			num++
+			time.Sleep(time.Second * SLEEPTIME)
+			go send(count, ip)
+		} else {
+			fmt.Println("=======================send Transaction end=========================")
+			break
+		}
+	}
+	fmt.Println("send Transaction num is:", num)
+}
+
+//send transaction init
+func send(count int, ip string) {
+	//dial giga
+	client, err := rpc.Dial("http://" + ip)
+
+	defer client.Close()
+
+	if err != nil {
+		fmt.Println("Dail:", ip, err.Error())
+		msg <- false
+		return
+	}
+
+	err = client.Call(&account, "eth_accounts")
+	if err != nil {
+		fmt.Println("eth_accounts Error", err.Error())
+		msg <- false
+		return
+	}
+	if len(account) == 0 {
+		fmt.Println("no account")
+		return
+	}
+	fmt.Println("account:", account)
+
+	// get balance
+	var result string
+	err = client.Call(&result, "eth_getBalance", account[from], "latest")
+	if err != nil {
+		fmt.Println("eth_getBalance Error:", err)
+		msg <- false
+		return
+	}
+
+	bl, _ := new(big.Int).SetString(result, 10)
+
+	fmt.Println("eth_getBalance Ok:", bl, result)
+
+	//unlock account
+	var reBool bool
+	err = client.Call(&reBool, "personal_unlockAccount", account[from], "admin", 90000)
+	if err != nil {
+		fmt.Println("personal_unlockAccount Error:", err.Error())
+		msg <- false
+		return
+	}
+
+	fmt.Println("personal_unlockAccount Ok", reBool)
+
+	fmt.Println("===========================")
+	// send
+	waitMain := &sync.WaitGroup{}
+	for {
+		waitMain.Add(1)
+		go sendTransactions(client, account, count, waitMain)
+		frequency--
+		if frequency <= 0 {
+			break
+		}
+		time.Sleep(interval)
+		// get balance
+		err = client.Call(&result, "eth_getBalance", account[from], "latest")
+		if err != nil {
+			fmt.Println("eth_getBalance Error:", err)
+			msg <- false
+			return
+		}
+
+		bl, _ := new(big.Int).SetString(result, 10)
+		fmt.Println("eth_getBalance Ok:", bl, result)
+
+	}
+	waitMain.Wait()
+	msg <- true
+}
+
+//send count transaction
+func sendTransactions(client *rpc.Client, account []string, count int, wait *sync.WaitGroup) {
+	defer wait.Done()
+	waitGroup := &sync.WaitGroup{}
+
+	for a := 0; a < count; a++ {
+		waitGroup.Add(1)
+		go sendTransaction(client, account, waitGroup)
+	}
+	fmt.Println("Send in go Complete", count)
+	waitGroup.Wait()
+	fmt.Println("Complete", Count)
+}
+
+//send one transaction
+func sendTransaction(client *rpc.Client, account []string, wait *sync.WaitGroup) {
+	defer wait.Done()
+	mapData := make(map[string]interface{})
+	mapData["from"] = account[from]
+
+	address := genAddress()
+	if to == 0 {
+		address = account[to]
+	}
+	mapData["to"] = address
+
+	mapData["value"] = "0x2100"
+	var result string
+	client.Call(&result, "eth_sendTransaction", mapData)
+	if result != "" {
+		Count++
+		fmt.Println("transaction result", result)
+	}
+}
+
+// Genesis address
+func genAddress() string {
+	priKey, _ := crypto.GenerateKey()
+	address := crypto.PubkeyToAddress(priKey.PublicKey)
+	return address.Hex()
+}
