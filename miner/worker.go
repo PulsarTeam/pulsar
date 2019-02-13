1e91f73c9b9115e9ba825cecd5f3b7c2ad417f54
felix <bytesdancer@gmail.com>
2019-02-13 16:46:31 +0800

    fix an error.

--- miner/worker.go
+++ miner/worker.go
@@ -577,8 +577,9 @@ func (self *worker) isReferenced(block *types.Block, ancestors []*types.Block) b
 
 //
 func (self *worker) canBackToPivot(block *types.Block, ancestors []*types.Block) bool {
+	b := block
 	for i := 0; i < len(ancestors); i++ {
-		b := self.chain.GetBlockByNumber(block.Number().Uint64() - uint64(i)) // block's parent
+		b := self.chain.GetBlockByHash(b.ParentHash())
 		if self.inAncestors(b, ancestors) {
 			return true
 		}
